5305195fb9b7fd5a70a5ceec11c6a85f5e22dc2c
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2020-04-12 21:13:31 +0200

    Update package json and webpack config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-'use strict'
-
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './test.css'
 
 const Index = () => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,114 @@
 'use strict'
 
 const path = require('path')
-const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
-const htmlPlugin = new HtmlWebpackPlugin({
-  inject: true,
-  template: './src/index.html',
-  filename: './index.html'
-})
+module.exports = (env, { mode }) => {
+  const isProduction = mode === 'production'
 
-const importReact = new webpack.ProvidePlugin({
-  'React': 'react',
-  'ReactDOM': 'react-dom'
-})
-
-const config = {
-  mode: process.env.NODE_ENV || 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'src/script.js'
-  },
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.(js|jsx)?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'standard-loader',
-          options: {
-            parser: 'babel-eslint'
-          }
-        }
-      },
-      {
-        test: /\.(js|jsx)?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+  const config = {
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      alias: {
+        src: path.resolve(__dirname, 'src/')
       }
+    },
+    mode: mode,
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'src/js/[name].js'
+    },
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /\.(js|jsx)?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'standard-loader',
+            options: {
+              parser: 'babel-eslint'
+            }
+          }
+        },
+        {
+          test: /\.(js|jsx)?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          }
+        },
+        {
+          test: /\.(png|jp(e*)g|svg)$/,
+          use: [{
+            loader: 'url-loader',
+            options: {
+              limit: 8000,
+              name: 'src/assets/images/[name].[ext]'
+            }
+          }]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: '../'
+              }
+            },
+            {
+              loader: 'css-loader'
+            }
+          ]
+        }
+      ]
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            parse: {
+              ecma: 8
+            },
+            compress: {
+              ecma: 5,
+              dead_code: true,
+              warnings: false,
+              comparisons: false,
+              inline: 2
+            },
+            mangle: {
+              safari10: true
+            },
+            output: {
+              ecma: 5,
+              comments: false,
+              ascii_only: true
+            }
+          },
+          cache: true
+        }),
+        new OptimizeCSSAssetsPlugin({})
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: isProduction ? '!!prerender-loader?string!./public/index.html' : './public/index.html',
+        filename: './index.html'
+      }),
+      new MiniCssExtractPlugin({
+          filename: 'src/css/[name].css'
+      })
     ]
-  },
-  plugins: [
-    htmlPlugin,
-    importReact
-  ]
+  }
+
+  return config
 }
-
-if (process.env.NODE_ENV === 'development') {
-  config.module.rules.push({
-    test: /\.css$/,
-    use: ['style-loader','css-loader']
-  })
-} else {
-  config.module.rules.push({
-    test: /\.css$/,
-    use: [
-      MiniCssExtractPlugin.loader,
-      'css-loader'
-    ]
-  })
-
-  config.plugins.push(new MiniCssExtractPlugin({ filename:'src/style.css' }))
-}
-
-module.exports = config
